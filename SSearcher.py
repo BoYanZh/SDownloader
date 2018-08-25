03bb7a7b152523c3e1e8b8d630acfaef29f5e5a1
zya <1328410180@qq.com>
2018-08-25 20:29:21 +0800

    performance improvement

--- SSearcher.py
+++ SSearcher.py
@@ -1,12 +1,14 @@
 from urllib.request import Request, urlopen
+from urllib.error import URLError
+import threading as td
 import random
 import re
-import platform
 import time
+import queue
 
 PATTERN = re.compile(r'成人|色|视频|电影|照片')
 
-def validate(url):
+def validate(url, q):
     """
     Judge whether url is the website we need.
     """
@@ -15,8 +17,18 @@ def validate(url):
         request = Request(url, headers=headers)
         html = urlopen(request, timeout=4).read().decode('utf-8')
         if re.search(PATTERN, html):
-            print(url)
+            q.put((url, html))
         else:
+            return
+    except UnicodeDecodeError:
+        try:
+            request = Request(url, headers=headers)
+            html = urlopen(request, timeout=4).read().decode('gbk', 'replace')
+            if re.search(PATTERN, html):
+                q.put((url, html))
+            else:
+                return
+        except:
             return
     except:
         return
@@ -30,10 +42,9 @@ def gen():
     case = random.choice([0,1,2])
     if case == 0:
         l=random.randint(0,9)
-        jl=random.randint(97,122)
         kl=random.randint(97,122)
         ll=random.randint(97,122)
-        return 'http://www.'+str(l)+str(l)+str(l)+chr(jl)+chr(kl)+chr(ll)+'.com'
+        return 'http://www.'+str(l)+str(l)+chr(kl)+chr(ll)+chr(kl)+chr(ll)+'.com'
     elif case == 1:
         l=random.randint(97,122)
         jl=random.randint(0,9)
@@ -45,20 +56,28 @@ def gen():
         kl=random.randint(0,9)
         jl=random.randint(0,9)
         return 'http://www.'+chr(l)+chr(l)+str(jl)+str(kl)+str(jl)+str(kl)+'.com'
-    
+
+
 PROCESSES = 64
 
 if __name__ == "__main__":
-    if platform.system() != 'Windows':
-        import multiprocessing as mp
-        pool = mp.Pool(processes=PROCESSES)
-        while True:
-            page_urls = [gen() for _ in range(1000)]
-            results = pool.map(validate, page_urls)
-    else:
-        import threading as td
-        base_active_count = td.active_count()
-        while True:
+    base_active_count = td.active_count()
+    url_set = set()
+    html_set = set()
+    q = queue.Queue()
+    while True:
+        try:
             while td.active_count() - base_active_count < PROCESSES:
-                p = td.Thread(target=validate, args=(gen(),)).start()
+                url = gen()
+                if url not in url_set:
+                    p = td.Thread(target=validate, args=(url, q)).start()
+                    url_set.update(url)
+                while not q.empty():
+                    url, html = q.get()
+                    if html not in html_set:
+                        print(url)
+                        html_set.update(html)
             time.sleep(0.01)
+
+        except KeyboardInterrupt:
+            break
